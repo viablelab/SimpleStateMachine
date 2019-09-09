1bf12f7092252ef831b4e250c23ba6768341addc
Johan <johan.west@viablelab.com>
2019-09-09 21:53:11 +0200

    Initial Commit

---
+++ SimpleStateMachine/SimpleStateMachine.h
@@ -0,0 +1,19 @@
+//
+//  SimpleStateMachine.h
+//  SimpleStateMachine
+//
+//  Created by Johan West on 2019-09-09.
+//  Copyright © 2019 Viable lab AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleStateMachine.
+FOUNDATION_EXPORT double SimpleStateMachineVersionNumber;
+
+//! Project version string for SimpleStateMachine.
+FOUNDATION_EXPORT const unsigned char SimpleStateMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleStateMachine/PublicHeader.h>
+
+
